51ae087aeb6460eddb94477df1e3285a8e7f78c0
Ayush-dvplr <ayush.dvlpr@gmail.com>
2024-02-09 10:15:40 +0530

    final changes

--- src/main/java/com/BCS10322/JDBC/ConnectionClass.java
+++ src/main/java/com/BCS10322/JDBC/ConnectionClass.java
@@ -5,7 +5,7 @@ public class ConnectionClass {
 	public Connection getConnection() throws ClassNotFoundException, SQLException {
 		Connection con=null;
 		Class.forName("com.mysql.cj.jdbc.Driver");//Load and register driver
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_22","root","Mysql@1701");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","Ayush@123");
 		System.out.println("Connection Established");
 		return con;
 	}
