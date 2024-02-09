2b92892d892b96b0dfa822c046c112872a2c4802
Ayush-dvplr <ayush.dvlpr@gmail.com>
2024-02-09 09:59:15 +0530

    started new project

---
+++ src/main/java/com/BCS10322/JDBC/JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.BCS10322.JDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BCS10322/JDBC/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BCS10322.JDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
